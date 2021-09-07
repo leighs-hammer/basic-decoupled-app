4fec80f23bc8e6e79309b29f65c97d9e35f2fedb
Leigh Barnes <l.barnes@weareeight.com>
2021-09-07 14:47:20 +0100

    Add CSP rules dynamic to shop query as per new reqs

--- next.config.js
+++ next.config.js
@@ -4,17 +4,25 @@ module.exports = {
     return [
       {
         source: "/(.*?)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://basic-decoupled-app.vercel.app https://*.myshopify.com",
+          },
+        ],
+      },
+      {
+        source: "/(.*?)",
         has: [
           {
             type: "query",
             key: "shop",
-            value: "(?<shop>[a-zA-Z0-9][a-zA-Z0-9-]*.myshopify.com[/]?)",
           },
         ],
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' https://basic-decoupled-app.vercel.app https://*.myshopify.com https://:shop;",
+            value: "frame-ancestors 'self' https://basic-decoupled-app.vercel.app https://:shop;",
           },
         ],
       },
